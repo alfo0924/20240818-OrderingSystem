42671fe78b9eb2dae62535e380947296b23155d4
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-18 22:39:12 +0800

    v1.12 proj
    cnp

--- src/main/java/fcu/web/_20240818orderingsystem/Application.java
+++ src/main/java/fcu/web/_20240818orderingsystem/Application.java
@@ -2,8 +2,10 @@ package fcu.web._20240818orderingsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "fcu.web._20240818orderingsystem")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
