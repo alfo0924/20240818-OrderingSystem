0ba3f011eff5ed37282df391f5b58b32ca51b0a1
邱勇嘉 <d1204429@o365.fcu.edu.tw>
2024-08-22 01:29:38 +0800

    新增版面與list資料

---
+++ src/main/resources/static/js/order.js
@@ -0,0 +1,15 @@
+const restaurants = [
+    "restaurant1",
+    "restaurant2",
+    "restaurant3",
+    "restaurant4",
+    "restaurant5",
+    "restaurant6"
+]
+
+const restaurant1 = [
+    "Latte Coffice 100"
+
+
+
+    ]
