ad31cd686a36d75f1179dfad506f2eea197c4300
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-08-24 17:12:57 +0800

    Delete src/main directory

--- src/main/java/fcu/web/_20240818orderingsystem/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package fcu.web._20240818orderingsystem.repository;
-
-import fcu.web._20240818orderingsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- src/main/java/fcu/web/_20240818orderingsystem/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package fcu.web._20240818orderingsystem.service;
-
-import fcu.web._20240818orderingsystem.model.Product;
-import java.util.List;
-
-public interface ProductService {
-    List<Product> getAllProducts();
-    Product getProductById(Long id);
-}
